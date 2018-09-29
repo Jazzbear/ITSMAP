7605c1175fdf866c8caffa7c1774adaa10d00a95
Asbjørn T. Gyring <assi90@gmail.com>
2018-09-29 17:34:35 +0200

    Changes to the image view. Fixed it so it uses icons instead which is better as it will scale more correctly on devices with different pixel density.

--- AssignmentOne/app/src/main/java/com/example/jazzbear/assignmentone/OverviewActivity.java
+++ AssignmentOne/app/src/main/java/com/example/jazzbear/assignmentone/OverviewActivity.java
@@ -3,6 +3,7 @@ package com.example.jazzbear.assignmentone;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.drawable.Icon;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -75,8 +76,6 @@ public class OverviewActivity extends AppCompatActivity {
                 stock = data.getParcelableExtra(STOCKOBJECT_EXTRA);
                 updateUI(stock);
                 toast("OK");
-//                stock = data.getParcelableExtra()
-//                updateUI();
             } else {
                 toast("No changes made");
             }
@@ -88,7 +87,6 @@ public class OverviewActivity extends AppCompatActivity {
         String purchaseString = "Purchased at: " + input.getStockPrice();
         stockPurchasePrice.setText(purchaseString);
         setImageView(input);
-//        toast("UI Updated");
     }
 
     private void setImageView(Stock input) {
@@ -98,14 +96,18 @@ public class OverviewActivity extends AppCompatActivity {
         String materialSector = res.getString(R.string.sectorMats);
         String healthSector = res.getString(R.string.sectorHealth);
 
-
+        //commented out the setImageDrawable since its better to use icons as they scale in pixel density for each device.
+        // but left them there to show the alternative.
         if (sectorValue != null) {
             if (sectorValue.equalsIgnoreCase(techSector)) {
-                imgView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.technology));
+                imgView.setImageResource(R.mipmap.ic_technology_foreground);
+//                imgView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.technology));
             } else if (sectorValue.equalsIgnoreCase(materialSector)) {
-                imgView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.materials));
+                imgView.setImageResource(R.mipmap.ic_materials_foreground);
+//                imgView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.materials));
             } else if (sectorValue.equalsIgnoreCase(healthSector)) {
-                imgView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.healthcare));
+                imgView.setImageResource(R.mipmap.ic_healthcare_foreground);
+//                imgView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.healthcare));
             }
         }
     }
