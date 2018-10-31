70337982aa346e1c2bbe5ca9f23962907630681b
Asbjørn T. Gyring <assi90@gmail.com>
2018-10-31 13:58:07 +0100

    Removed unused project

--- Assignment2StockMonitor/app/src/main/java/com/example/jazzbear/assignment2_stockmonitor/Utils/Globals.java
+++
@@ -1,8 +0,0 @@
-package com.example.jazzbear.assignment2_stockmonitor.Utils;
-
-public class Globals {
-    public static final int DETAILS_REQUEST = 101;
-    public static final int EDIT_REQUEST = 201;
-    public static final String STOCKOBJECT_EXTRA = "com.jazzbear.stockobject";
-    public static final String STOCK_STATE = "state_of_stock";
-}
